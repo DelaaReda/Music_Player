cd4c1884c766907905ab82a7c876fbdbec7a04c4
stephanecharron <stephanecharron@this.com>
2018-02-03 23:27:38 -0500

    player play stop pause loadPlaylist

--- test/MusicPayerTest.js
+++ test/MusicPayerTest.js
@@ -6,4 +6,21 @@ describe("Music player", function () {
     it("should be define", function () {
         expect(new MusicPlayer()).not.to.be.undefined;
     });
+
+    describe("Music player functionnality play", function () {
+        it("should be defined", function () {
+            expect(new MusicPlayer().play).not.to.be.undefined;
+        });
+    });
+    describe("Music player functionnality stop", function () {
+        it("should be defined", function () {
+            expect(new MusicPlayer().stop).not.to.be.undefined;
+        });
+        it("should be defined", function () {
+            expect(new MusicPlayer().pause).not.to.be.undefined;
+        });
+        it("should be defined", function () {
+            expect(new MusicPlayer().loadPlayList).not.to.be.undefined;
+        });
+    });
 });
