4f8ea276fafda5b337b25852ff39d2bf98959d36
MrReda <reda@bunji.ca>
2018-02-03 23:44:15 -0500

    Version 0

--- app/MusicPlayer.js
+++ app/MusicPlayer.js
@@ -2,3 +2,16 @@ function MusicPlayer() {
 }
 
 module.exports = MusicPlayer;
+
+MusicPlayer.prototype.play = function() {
+    this.playing = "playing";
+};
+MusicPlayer.prototype.loadPlayList= function() {
+    this.PlayList = "loaded";
+};
+MusicPlayer.prototype.pause= function() {
+    this.pauseStatus = true;
+};
+MusicPlayer.prototype.stop= function() {
+    this.stopped = true;
+};
