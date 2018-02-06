227b7b0fd8cb830c2f5750da9850f8571c27ab84
MrReda <reda@bunji.ca>
2018-02-05 20:52:58 -0500

    Set up Events : play, pause, loadPlaylist, stop.

--- app/MusicPlayer.js
+++ app/MusicPlayer.js
@@ -1,17 +1,51 @@
-function MusicPlayer() {
-}
+var util = require('util');
+var events = require('events');
 
-module.exports = MusicPlayer;
+function MusicPlayer() {
+    this.playList = false;
+    this.nowPlaying = false;
+    this.pauseStatus = false;
+    this.stopped = false;
+
+    events.EventEmitter.call(this);
+    this.on(MusicPlayer.events.play, this.play.bind(this));
+    this.on(MusicPlayer.events.loadPlayList, this.loadPlayList.bind(this));
+    this.on(MusicPlayer.events.pause, this.pause.bind(this));
+    this.on(MusicPlayer.events.stop, this.stop.bind(this));
+}
+util.inherits(MusicPlayer, events.EventEmitter);
+
 
 MusicPlayer.prototype.play = function() {
-    this.playing = "playing";
+    this.playing = nowPlaying;
 };
 MusicPlayer.prototype.loadPlayList= function() {
-    this.PlayList = "loaded";
+    this.PlayList = playList;
 };
 MusicPlayer.prototype.pause= function() {
-    this.pauseStatus = true;
+    this.pauseStatus = Pause;
 };
 MusicPlayer.prototype.stop= function() {
-    this.stopped = true;
+    this.stopped = stopStat;
 };
+var e = MusicPlayer.events = {
+    play: 'play',
+    loadPlayList: 'loadPlayList',
+    pause: 'pause',
+    stop: 'stop'
+};
+var musicPlayer = new MusicPlayer();
+
+musicPlayer.on(e.play, function() {
+    this.nowPlaying = true;
+});
+musicPlayer.on(e.loadPlayList, function() {
+    this.playList = true;
+});
+musicPlayer.on(e.pause, function() {
+    this.pauseStatus = true;
+});
+musicPlayer.on(e.stop, function() {
+    this.stopped = true;
+});
+module.exports = MusicPlayer;
